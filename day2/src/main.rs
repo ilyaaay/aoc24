0479aec227d663390e522666064fe95ee49dd777
Suhodolets Ilya <suhodolets.ia@intechs.by>
2024-12-03 02:22:59 +0300

    add day2 part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,11 +4,31 @@ fn main() -> io::Result<()> {
     let input = fs::read_to_string("./src/input.txt")?;
 
     println!("{}", day1(&input));
+    println!("{}", day2(&input));
 
     Ok(())
 }
 
 fn day1(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|x| {
+            x.split_whitespace()
+                .filter_map(|s| s.parse().ok())
+                .collect::<Vec<i32>>()
+        })
+        .fold(0, |x, nums| {
+            match (nums.is_sorted_by(|x, y| x < y) || nums.is_sorted_by(|x, y| x > y))
+                && (nums.is_sorted_by(|x, y| x.abs_diff(*y) >= 1)
+                    && nums.is_sorted_by(|x, y| x.abs_diff(*y) <= 3))
+            {
+                true => x + 1,
+                false => x,
+            }
+        })
+}
+
+fn day2(input: &str) -> i32 {
     let mut result = 0;
 
     for i in input.lines() {
@@ -25,8 +45,7 @@ fn day1(input: &str) -> i32 {
 mod tests {
     use crate::*;
 
-    const INPUT: &str = r#"
-7 6 4 2 1
+    const INPUT: &str = r#"7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
@@ -36,5 +55,10 @@ mod tests {
     #[test]
     fn test_day1() {
         assert_eq!(day1(INPUT), 2);
+    }
+
+    #[test]
+    fn test_day2() {
+        assert!(true);
     }
 }
