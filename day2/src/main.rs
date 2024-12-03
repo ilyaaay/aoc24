11d369c550655f5a8b51baba0b7eb60036b36f87
Suhodolets Ilya <iliasuhodolets@gmail.com>
2024-12-03 20:58:32 +0300

    day2 part2 (WIP)

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,15 +1,15 @@
-use std::{fs, io};
+use std::{collections::HashMap, fs, io};
 
 fn main() -> io::Result<()> {
     let input = fs::read_to_string("./src/input.txt")?;
 
-    println!("{}", day1(&input));
-    println!("{}", day2(&input));
+    println!("{}", first_part(&input));
+    println!("{}", second_part(&input));
 
     Ok(())
 }
 
-fn day1(input: &str) -> i32 {
+fn first_part(input: &str) -> i32 {
     input
         .lines()
         .map(|x| {
@@ -19,8 +19,7 @@ fn day1(input: &str) -> i32 {
         })
         .fold(0, |x, nums| {
             match (nums.is_sorted_by(|x, y| x < y) || nums.is_sorted_by(|x, y| x > y))
-                && (nums.is_sorted_by(|x, y| x.abs_diff(*y) >= 1)
-                    && nums.is_sorted_by(|x, y| x.abs_diff(*y) <= 3))
+                && (nums.is_sorted_by(|x, &y| x.abs_diff(y) >= 1 && x.abs_diff(y) <= 3))
             {
                 true => x + 1,
                 false => x,
@@ -28,7 +27,7 @@ fn day1(input: &str) -> i32 {
         })
 }
 
-fn day2(input: &str) -> i32 {
+fn second_part(input: &str) -> i32 {
     let mut result = 0;
 
     for i in input.lines() {
@@ -41,24 +40,19 @@ fn day2(input: &str) -> i32 {
     result
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::*;
-
-    const INPUT: &str = r#"7 6 4 2 1
+const INPUT: &str = r#"7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9"#;
 
-    #[test]
-    fn test_day1() {
-        assert_eq!(day1(INPUT), 2);
-    }
+#[test]
+fn test_first_part() {
+    assert_eq!(first_part(INPUT), 2);
+}
 
-    #[test]
-    fn test_day2() {
-        assert!(true);
-    }
+#[test]
+fn test_second_part() {
+    assert_eq!(second_part(INPUT), 4);
 }
